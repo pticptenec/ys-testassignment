44aba7ca9d4c94367d705dcc5cd36c9bc030328c
pticptenec <84808603+pticptenec@users.noreply.github.com>
2022-03-19 16:47:30 +0300

    db schema init migration

--- testdb/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-from pymongo import MongoClient
-
-
-client = MongoClient(os.getenv('MONGO_URI'))
-
-songs_db = client['songs_db']
-songs = songs_db['songs']
-songs.create_index({'artist': 'text', 'title': 'text'})
---
+++ testdb/__main__.py
@@ -0,0 +1,12 @@
+import os
+from pymongo import MongoClient
+from pymongo import TEXT
+
+client = MongoClient(os.getenv('MONGO_URI'))
+
+songs_db = client['songs_db']
+songs = songs_db['songs']
+songs.drop_indexes()
+songs.insert_one({"artist": "The Yousicians","title": "Lycanthropic Metamorphosis","difficulty": 14.6,"level":13,"released": "2016-10-26"})
+songs.create_index([('artist', TEXT), ('title', TEXT)])
+songs.delete_many({})
